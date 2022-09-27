f50273cc06497fce0ab8dd3bf9b2ad016277bf8c
yadavravi043 <ravi18016925@gmail.com>
2022-09-27 16:50:47 +0530

    fix

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ io.on("connection", (socket) => {
   socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
-      //socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+      //socket.to(sendUserSocket).emit("msg-recieve", data.message);
     }
   });
 });
